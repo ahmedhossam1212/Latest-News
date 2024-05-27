8656d294d26cdaa5955a50e16d462c0fc23ebb2a
ahmedhossam1212 <ahossamm404@gmail.com>
2024-05-27 11:12:42 +0300

    add colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:latest_news/core/utils/hex_colors.dart';
 
 class AppColors {
-  static Color lightBlack = HexColor("#555667");
-  static Color red = HexColor("#E64852");
+  static Color yellow = HexColor("#F98121");
   static Color white = HexColor("FFFFFF");
-  static Color grey = HexColor("#F3F5F9");
-  static Color black = HexColor("#1D1D1F");
-  static Color lightGrey = HexColor("#C4C4C4");
+  static Color grey = HexColor("#828282");
+  static Color black = HexColor("#000000");
+  static Color lightGrey = HexColor("#F2F2F2");
 }
